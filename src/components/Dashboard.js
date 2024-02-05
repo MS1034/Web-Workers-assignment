1058585c3eaefb9a393271f4e670a27fdd1e025d
Muhammad Subhan <sbse1034@gmail.com>
2024-02-05 19:03:03 +0500

    Fix  Eslint error

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useContext, useEffect, useState } from "react";
 import ThemeContext from "../context/ThemeContext";
 import Details from "./Detail";
